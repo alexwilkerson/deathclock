512a8c9cd3c75bcefe5f5b24959af7e077ce5808
Alex Wilkerson <alex@uncorrected.com>
2019-05-26 17:14:52 -0500

    doneish?

--- main.go
+++ main.go
@@ -6,18 +6,22 @@ import (
 	"log"
 	"math"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jroimartin/gocui"
+	"github.com/pkg/errors"
 )
 
 var (
-	width, height int
-	hour          int
-	seconds       bool
-	done          = make(chan struct{})
-	logger        *log.Logger
-	color         string
+	width, height           int
+	year, day, hour, minute int
+	month                   time.Month
+	seconds                 bool
+	done                    = make(chan struct{})
+	logger                  *log.Logger
+	color                   string
 )
 
 func layout(g *gocui.Gui) error {
@@ -34,10 +38,10 @@ func layout(g *gocui.Gui) error {
 
 func counterString() string {
 	now := time.Now()
-	t := time.Date(now.Year(), now.Month(), now.Day(), hour, 0, 0, 0, now.Location())
+	t := time.Date(year, month, day, hour, minute, 0, 0, now.Location())
 
 	var h, m, s int
-	if time.Now().Hour() < hour {
+	if time.Until(t).Seconds() > 0 {
 		h = int(math.Floor(time.Until(t).Hours()))
 		if seconds {
 			m = int((time.Until(t).Hours() - math.Floor(time.Until(t).Hours())) * 60)
@@ -97,21 +101,81 @@ func printCounter(g *gocui.Gui, toPrint string) error {
 }
 
 func init() {
+	var endTime string
 	flag.StringVar(&color, "c", "red", "color of the countdown timer")
 	flag.BoolVar(&seconds, "s", false, "display countdown timer with seconds")
+	flag.StringVar(&endTime, "t", "5:00pm", "time to count down to in format: HH:MM(am|pm) (eg. \"5:00pm\")")
 	flag.Parse()
 
 	logger = log.New(os.Stdout, "deathclock:", log.LstdFlags)
+
+	if len(endTime) < 6 || len(endTime) > 7 {
+		logger.Println("t string should be in format: HH:MM(am|pm) (eg. \"5:00pm\")")
+		os.Exit(1)
+	}
+	period := endTime[len(endTime)-2:]
+	period = strings.ToLower(period)
+	if period != "pm" && period != "am" {
+		logger.Println("t string should be in format: HH:MM(am|pm) (eg. \"5:00pm\")")
+		os.Exit(1)
+	}
+	t := strings.Split(endTime[:len(endTime)-2], ":")
+	if len(t) != 2 {
+		logger.Println("t string should be in format: HH:MM(am|pm) (eg. \"5:00pm\")")
+		os.Exit(1)
+	}
+	hs := t[0]
+	ms := t[1]
+	h, err := strconv.Atoi(hs)
+	if err != nil {
+		logger.Println("t string should be in format: HH:MM(am|pm) (eg. \"5:00pm\")")
+		logger.Printf("%+v", errors.WithStack(err))
+		os.Exit(1)
+	}
+	if h < 1 || h > 12 {
+		logger.Println("t string should be in format: HH:MM(am|pm) (eg. \"5:00pm\")")
+		os.Exit(1)
+	}
+	if period == "pm" {
+		h += 12
+	}
+	m, err := strconv.Atoi(ms)
+	if err != nil {
+		logger.Println("t string should be in format: HH:MM(am|pm) (eg. \"5:00pm\")")
+		logger.Printf("%+v", errors.WithStack(err))
+		os.Exit(1)
+	}
+	if m < 0 || m > 59 {
+		logger.Println("t string should be in format: HH:MM(am|pm) (eg. \"5:00pm\")")
+		os.Exit(1)
+	}
+
+	now := time.Now()
+	parsedTime := time.Date(now.Year(), now.Month(), now.Day(), h, m, 0, 0, now.Location())
+
+	if int64(time.Until(parsedTime)) <= int64(0) {
+		parsedTime = parsedTime.AddDate(0, 0, 1)
+	}
+
+	logger.Println(time.Until(parsedTime))
+
+	year = parsedTime.Year()
+	month = parsedTime.Month()
+	day = parsedTime.Day()
+	hour = parsedTime.Hour()
+	minute = parsedTime.Minute()
+
+	logger.Println(now.Year(), now.Month(), now.Day(), h, m)
+	logger.Println(year, month, day, hour, minute)
 }
 
 func main() {
 	if seconds {
-		width = 48
+		width = 49
 	} else {
-		width = 30
+		width = 31
 	}
 	height = 5
-	hour = 17
 
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
