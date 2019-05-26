208a61cdbb830d01956d16485575b5314528f0b6
Alex Wilkerson <alex@uncorrected.com>
2019-05-26 15:40:54 -0500

    added flag for seconds

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math"
@@ -10,41 +11,15 @@ import (
 )
 
 var (
-	hour                    int
-	hours, minutes, seconds bool
-	done                    = make(chan struct{})
+	width, height int
+	hour          int
+	seconds       bool
+	done          = make(chan struct{})
 )
-
-func main() {
-	hour = 17
-
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
-
-	g.FgColor = gocui.ColorRed
-
-	g.SetManagerFunc(layout)
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'q', gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	go counter(g)
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-}
 
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("ctr", maxX/2-15, maxY/2-3, maxX/2+16, maxY/2+3); err != nil {
+	if v, err := g.SetView("ctr", maxX/2-int(math.Ceil(float64(width)/2)), maxY/2-int(math.Ceil(float64(height)/2)), maxX/2+int(math.Ceil(float64(width)/2)), maxY/2+int(math.Ceil(float64(height)/2))); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -58,18 +33,23 @@ func counterString() string {
 	now := time.Now()
 	t := time.Date(now.Year(), now.Month(), now.Day(), hour, 0, 0, 0, now.Location())
 
-	var h, m int
-	if time.Now().Hour() >= hour {
-		h = 0
-		m = 0
-	} else {
+	var h, m, s int
+	if time.Now().Hour() < hour {
 		h = int(math.Floor(time.Until(t).Hours()))
-		m = int(math.Ceil((time.Until(t).Hours() - math.Floor(time.Until(t).Hours())) * 60))
-	}
-	if m == 60 {
-		m = 0
+		if seconds {
+			m = int((time.Until(t).Hours() - math.Floor(time.Until(t).Hours())) * 60)
+			s = int((time.Until(t).Minutes() - math.Floor(time.Until(t).Minutes())) * 60)
+		} else {
+			m = int(math.Ceil((time.Until(t).Hours() - math.Floor(time.Until(t).Hours())) * 60))
+			if m == 60 {
+				m = 0
+			}
+		}
 	}
 
+	if seconds {
+		return fmt.Sprintf("%d:%02d:%02d", h, m, s)
+	}
 	return fmt.Sprintf("%d:%02d", h, m)
 }
 
@@ -111,4 +91,42 @@ func printCounter(g *gocui.Gui, toPrint string) error {
 	}
 
 	return nil
+}
+
+func init() {
+	flag.BoolVar(&seconds, "s", false, "display countdown timer with seconds")
+	flag.Parse()
+}
+
+func main() {
+	if seconds {
+		width = 48
+	} else {
+		width = 30
+	}
+	height = 5
+	hour = 17
+
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.FgColor = gocui.ColorRed
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", 'q', gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	go counter(g)
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
 }
